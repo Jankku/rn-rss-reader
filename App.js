e806494ed8c4b23aa442c7f8ee1ab2858b8755ec
Jankku <29043938+Jankku@users.noreply.github.com>
2022-02-09 20:12:46 +0200

    feat: get user location

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
+import LocationProvider from './data/LocationProvider';
 
 export default function App() {
+  const [region, setRegion] = useState();
+  const [errorMsg, setErrorMsg] = useState();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+          setErrorMsg('Permission to access location was denied');
+          return;
+        }
+
+        const { latitude, longitude } = await LocationProvider.getLocation();
+      } catch (error) {
+        setErrorMsg(error.message);
+      }
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>Region: {region ? region : '-'}</Text>
+      {errorMsg ? alert(errorMsg) : null}
     </View>
   );
 }
---
+++ data/LocationProvider.js
@@ -0,0 +1,49 @@
+import { LOCATION_API_KEY, LOCATION_BASE_URL } from '@env';
+import * as Location from 'expo-location';
+
+const LocationProvider = {
+  /**
+   * First tries to return last known location.
+   * If it's not available, get current location with lowest accuracy.
+   * @returns User location
+   */
+  async getLocation() {
+    const lastKnownPosition = await Location.getLastKnownPositionAsync({});
+
+    if (lastKnownPosition === null) {
+      const currentLocation = await Location.getCurrentPositionAsync({
+        mayShowUserSettingsDialog: false,
+        accuracy: Location.LocationAccuracy.Lowest,
+      });
+
+      return {
+        latitude: currentLocation.coords.latitude,
+        longitude: currentLocation.coords.longitude,
+      };
+    } else {
+      return {
+        latitude: lastKnownPosition.coords.latitude,
+        longitude: lastKnownPosition.coords.longitude,
+      };
+    }
+  },
+
+  /**
+   * @param {number} latitude
+   * @param {number} longitude
+   * @returns Region response JSON
+   */
+  async getRegion(latitude, longitude) {
+    if (__DEV__) {
+      return 'North Ostrobothnia';
+    } else {
+      return await fetch(
+        `${LOCATION_BASE_URL}/reverse.php?key=${LOCATION_API_KEY}&lat=${latitude}&lon=${longitude}&zoom=8&format=json`
+      )
+        .then((res) => res.json())
+        .then((json) => json.address.county);
+    }
+  },
+};
+
+export default LocationProvider;
